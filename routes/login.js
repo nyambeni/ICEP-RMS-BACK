66aad12b372b5a754170570336023cb12328489e
Themba-maker <thembantuli2018@gmail.com>
2020-02-20 11:51:31 +0200

    Themba

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,32 @@
+
+const mysql = require('mysql');
 const express = require('express');
-router = express.Router();
-con = require('../conn/conn');
+
+const mysqlConn= require('../conn/conn');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+	
+router.get('/login', function(request, response) {
+	
+	
+	var email = request.body.email;
+	var pwd = request.body.pwd;
+	
+
+	
+		mysqlConn.query('SELECT * FROM landlord WHERE email = ? AND pwd = ?', [email, pwd], function(error, results, fields) {
+            if (results.length > 0) {
+                response.send(results);
+                
+			}else {
+				response.send('Incorrect Email and/or Password!');
+			}			   
+			
+		});
+	
+
+});
 
 
-
-module.exports = router;
+module.exports = router ;
